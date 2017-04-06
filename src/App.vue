cb22444d34988e399030b52dd2d19e3ac9f29a90
Ratanraj <ratanraj.r@gmail.com>
2017-04-06 12:47:08 +0530

    Day 1

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <a-hello></a-hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    'a-hello': Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input v-model="classname">
+    <h2 v-bind:class="classname">{{message}}</h2>
+    <h2 class="redelement"></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data: function(){
+      return {
+          message: 'Hello! This is a component!',
+          classname: 'redelement'
+      }
+  }
+}
+</script>
+
+<style scoped>
+  .redelement {
+    color: red;
+  }
+  .blueelement {
+    color: blue;
+    border: 1px solid blue;
+  }
+</style>
